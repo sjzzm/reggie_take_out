c4068355a0fc2797e3713ae3429867b31776a793
zhiming zhou <zzhou90@sheffield.ac.uk>
2022-07-21 18:03:42 +0100

    setmeal

--- src/main/java/com/itheima/reggie/service/DishService.java
+++ src/main/java/com/itheima/reggie/service/DishService.java
@@ -1,7 +1,16 @@
 package com.itheima.reggie.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.itheima.reggie.dto.DishDto;
 import com.itheima.reggie.entity.Dish;
 
 public interface DishService extends IService<Dish> {
+
+    //新增菜品，同时插入菜品对应的口味数据，需要操作两张表，dish dishflavor
+    public void saveWithFlavor(DishDto dishDto);
+
+    public DishDto getByIdWithFlavor(Long id);
+
+    //更新菜品信息，也要更新口味信息
+    void updateWithFlavor(DishDto dishDto);
 }
--- src/main/java/com/itheima/reggie/service/impl/DishServiceImpl.java
+++ src/main/java/com/itheima/reggie/service/impl/DishServiceImpl.java
@@ -1,13 +1,94 @@
 package com.itheima.reggie.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.itheima.reggie.common.R;
+import com.itheima.reggie.dto.DishDto;
 import com.itheima.reggie.entity.Dish;
+import com.itheima.reggie.entity.DishFlavor;
 import com.itheima.reggie.mapper.DishMapper;
+import com.itheima.reggie.service.DishFlavorService;
 import com.itheima.reggie.service.DishService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
 public class DishServiceImpl extends ServiceImpl<DishMapper, Dish> implements DishService {
+    /**
+     * 新增菜品，同时保存口味数据
+     * @param dishDto
+     */
+
+    @Autowired
+    private DishFlavorService dishFlavorService;
+    @Override
+    @Transactional
+    public void saveWithFlavor(DishDto dishDto) {
+
+        //保存菜品基本信息到菜品表dish
+        this.save(dishDto);
+
+        Long dishID = dishDto.getId();
+
+        //菜品口味
+        List<DishFlavor> flavors = dishDto.getFlavors();
+        flavors = flavors.stream().map((item)->{
+            item.setDishId(dishID);
+            return item;
+        }).collect(Collectors.toList());
+
+        dishFlavorService.saveBatch(flavors);
+    }
+
+    @Override
+    public DishDto getByIdWithFlavor(Long id) {
+        //查询菜品基本信息，从dish表查
+        Dish dish = this.getById(id);
+
+        DishDto dishDto = new DishDto();
+
+        BeanUtils.copyProperties(dish, dishDto);
+
+        //查询菜品口味信息，从dishFlavor表查
+        LambdaQueryWrapper<DishFlavor> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(DishFlavor::getDishId, dish.getId());
+
+        List<DishFlavor> list = dishFlavorService.list(queryWrapper);
+
+        dishDto.setFlavors(list);
+
+        return dishDto;
+    }
+
+    @Override
+    @Transactional
+    public void updateWithFlavor(DishDto dishDto) {
+        //先更新dish基本菜品信息
+        this.updateById(dishDto);
+
+        //清理当前菜品对应口味数据--dish_flavor表的delete操作
+        LambdaQueryWrapper<DishFlavor> queryWrapper = new LambdaQueryWrapper<>();
+
+        queryWrapper.eq(DishFlavor::getDishId, dishDto.getId());
+
+        dishFlavorService.remove(queryWrapper);
+
+        //添加当前提交过来的 口味数据 --dish_flavor表的insert操作
+
+        List<DishFlavor> flavors = dishDto.getFlavors();
+
+        flavors.stream().map((item -> {
+            item.setDishId(dishDto.getId());
+            return item;
+        })).collect(Collectors.toList());
+
+        dishFlavorService.saveBatch(flavors);
+    }
 }
